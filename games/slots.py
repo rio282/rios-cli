0c989092b3e007c7be5b1f35551da460af4c3714
Rio Bérénos <r.berenos@hotmail.com>
2024-08-07 19:24:37 +0200

    Update slots.py

--- games/slots.py
+++ games/slots.py
@@ -2,6 +2,8 @@ import curses
 import random
 from time import sleep
 
+from colorama import Fore
+
 
 class Slots:
     def __init__(self):
@@ -11,6 +13,8 @@ class Slots:
         self.longest_symbol = len(max(self.symbols, key=len))
         self.__empty_symbol = " " * self.longest_symbol
         self.results = [self.__empty_symbol for _ in range(self.num_reels)]
+
+        self.spinning = False
 
     def draw_slot_machine(self, stdscr):
         stdscr.clear()
@@ -22,31 +26,56 @@ class Slots:
         stdscr.addstr(height - 2, 0, "Press '<SPACE>' to spin, 'q' to quit")
         stdscr.refresh()
 
-    def spin_slots(self):
+    def spin_slots(self, stdscr):
+        self.spinning = True
+
+        total_duration = 1.75
+        num_frames = 50
+        max_delay = total_duration / num_frames
+        min_delay = 0.05
+
+        # DA SPIN INNIT
+        for frame in range(num_frames):
+            delay = max_delay - (max_delay - min_delay) * (frame / num_frames)
+
+            for i in range(self.num_reels):
+                self.results[i] = str(random.choice(self.symbols)).center(self.longest_symbol)
+            self.draw_slot_machine(stdscr)
+
+            sleep(delay)
+
+        # FINAL RESULTS
         for i in range(self.num_reels):
-            result = str(random.choice(self.symbols)).center(self.longest_symbol)
-            self.results[i] = result
+            self.results[i] = str(random.choice(self.symbols)).center(self.longest_symbol)
+        self.draw_slot_machine(stdscr)
+        sleep(0.5)
+
+        self.spinning = False
 
     def play(self):
         def inner(stdscr):
             curses.curs_set(0)
             stdscr.nodelay(1)
-            stdscr.timeout(100)
 
             while True:
                 self.draw_slot_machine(stdscr)
-                key = stdscr.getch()
 
-                if key == ord('q'):
-                    break
-                elif key == ord(' '):
-                    self.spin_slots()
-                    self.draw_slot_machine(stdscr)
-                    sleep(0.5)
+                # FIXME: yo why tf is this not working, am i tweaking?
+                if not self.spinning:
+                    key = stdscr.getch()
+                    if key == ord('q'):
+                        break
+                    elif key == ord(' '):
+                        self.spin_slots(stdscr)
 
                 if len(set(self.results)) == 1 and self.results[0] != self.__empty_symbol:
                     stdscr.addstr(0, 0, "JACKPOT! Press any key to continue...")
+                    stdscr.refresh()
                     stdscr.getch()
                     break
 
-        curses.wrapper(inner)
+        win = curses.wrapper(inner)
+        if win:
+            print(f"{Fore.GREEN}You won the JACKPOT!!!")
+        else:
+            print(F"{Fore.RED}Better luck next time!")
